3e6b060eaff58cfab0b2cd35107f5ff62ec84b15
Marco <marco.kosters@hva.nl>
2022-09-13 13:07:39 +0200

    bug fix modal
    nu kan je 5 verschillende modal windows hebben ipv 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,40 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var buttons = document.querySelectorAll(".js-open-modal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelector(".close");
 
+console.log(buttons)
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+
+var active
+buttons.forEach(function (button) {
+    console.log(button)
+    button.addEventListener("click", function () {
+        console.log("test")
+        modal.style.display = "block"
+        active = document.getElementById(button.dataset.opens)
+        active.classList.add("active")
+    })
+
+})
+
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
+    active.classList.remove("active")
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        active.classList.remove("active")
+    }
 }
