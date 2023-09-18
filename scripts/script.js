1b3226a89b84fe8dd9ce98d4e5326f1fd942fe59
Patrickkhr <patrickhrdjk@gmail.com>
2023-09-18 10:25:59 +0200

    Button aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
+const arrowBtns = document.querySelectorAll(".wrapper button");
 const carouselChildrens = [...carousel.children];
 
 // Add event listeners for the arrow buttons to scroll the carousel left and right
