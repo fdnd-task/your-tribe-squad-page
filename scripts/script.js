31124e8b5c7ea8d412cd7d8867fd9255a2b3950f
RobbyRisse <robby.risse@gmail.com>
2023-09-21 10:14:19 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const wrapper = document.querySelector(".wrapper");
-    const carousel = document.querySelector(".carousel");
-    const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-    const arrowBtns = document.querySelectorAll(".wrapper button");
-  
-    // Add event listeners for the arrow buttons to scroll the carousel left and right
-    arrowBtns.forEach(btn => {
-      btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-      });
-    });
-  
-    function randomizeItems(items) {
-      var cached = items.slice(0), temp, i = cached.length, rand;
-      while (--i) {
-        rand = Math.floor(Math.random() * (i + 1));
-        temp = cached[rand];
-        cached[rand] = cached[i];
-        cached[i] = temp;
+  const wrapper = document.querySelector(".wrapper");
+  const carousel = document.querySelector(".carousel");
+  const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+  const arrowBtns = document.querySelectorAll(".wrapper button");
+
+  // Add event listeners for the arrow buttons to scroll the carousel left and right
+  arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      if (btn.id == "left") {
+        if (carousel.scrollLeft <= 0) {
+          // als we aan het begin zijn, scroll naar het einde.
+          carousel.scrollLeft = carousel.scrollWidth;
+        } else {
+          carousel.scrollLeft -= firstCardWidth;
+        }
+      } else {
+        if (carousel.scrollLeft + carousel.clientWidth + firstCardWidth >= carousel.scrollWidth) {
+          // als we aan het einde zijn, terug naar begin.
+          carousel.scrollLeft = 0;
+        } else {
+          carousel.scrollLeft += firstCardWidth;
+        }
       }
-      return cached;
-    }
-  
-    // Example: Call this function to randomize your card elements
-    function randomizeCardList() {
-      const cardList = document.querySelector(".carousel");
-      const cardItems = Array.from(cardList.querySelectorAll(".card"));
-      const randomizedItems = randomizeItems(cardItems);
-      cardList.innerHTML = ""; // Clear the existing list
-      randomizedItems.forEach(item => cardList.appendChild(item));
-    }
-  
-    // Call the randomizeCardList function to randomize your card elements
-    randomizeCardList();
+    });
   });
+});
